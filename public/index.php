eb878cb7712fb9ff817ab819fefe909ee1abff5e
lainver <092018amir@gmail.com>
2025-06-18 22:07:21 +0700

    gatau

--- public/index.php
+++ public/index.php
@@ -18,3 +18,6 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
+
+require __DIR__.'/../laravel_app/vendor/autoload.php';
+$app = require_once __DIR__.'/../laravel_app/bootstrap/app.php';
